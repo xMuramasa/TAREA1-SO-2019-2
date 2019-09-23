d29b5d2cfb839fe5b8066cd95d3b718ca021d897
Martin Salinas Scussolin <marthoxx@gmail.com>
2019-09-23 15:32:46 -0300

    creacion del deck completa

--- Test.c
+++ Test.c
@@ -1,18 +1,21 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
+//Variable con los colores de cartas
+char* cardNames[50] = {"rojo","azul","amarillo","verde","negro"};
 
-/* Void my_rmdir function
+/* Void myRmdir function
 *   funcion : remueve una carpeta
 *   prints  : si se elimina la carpeta correctamente, lo avisa
 *   retorna : nada
 */
-void my_rmdir(char* nombre_carpeta){
+void myRmdir(char* nombre_carpeta){
 
     int rm = rmdir(nombre_carpeta);
 
@@ -23,27 +26,62 @@ void my_rmdir(char* nombre_carpeta){
     }
 }
 
-/* Void my_mkdir function
+/* Void myMkdir function
 *   funcion : crea una carpeta
 *   prints  : si se crea la carpeta correctamente, lo avisa
 *   retorna : nada
 */
-void my_mkdir(char* nombre_carpeta){
-
+void myMkdir(char* nombre_carpeta){
     int mk = mkdir(nombre_carpeta, 0700);
 
     if (mk == 0){
         printf("El direcotrio %s fue creado correctamente.\n", nombre_carpeta);
     }else{
-        printf("No se pudo eliminar el direcotrio %s correctamente.\n", nombre_carpeta);
+        printf("No se pudo crear el direcotrio %s correctamente.\n", nombre_carpeta);
     }
+}
+
+void createDeck(){
+    int i,j,k;
+    char buffer[100];
+    myMkdir("Deck");
+    int filedesc; //= open("Deck/testfile.txt", O_CREAT|O_APPEND|O_RDWR);
+
+    for(i = 0;i < 4;i++){ //crea cartas 0 de cada color
+        
+        strcpy(buffer,"");
+        sprintf(buffer, "Deck/%s_0.txt", cardNames[i]);
+        
+        filedesc = open(buffer, O_CREAT|O_APPEND|O_RDWR);
+        if(filedesc < 0){
+            printf("Error al crear archivo: %s\n", buffer);
+        }
+        else{
+            close(filedesc);
+            printf("Se creo el archivo: %s\n", buffer);
+        }
+        
+        for(j = 1; j < 10; j++){ //crea cartas del 1 al 9 de cada color
+            for ( k = 1; k < 3; k++){
+                strcpy(buffer,"");
+                sprintf(buffer, "Deck/%s_%d_%d.txt", cardNames[i], j, k);
+                filedesc = open(buffer, O_CREAT|O_APPEND|O_RDWR);
+                if(filedesc < 0){
+                    printf("Error al crear archivo: %s\n", buffer);
+                }
+                else{
+                    close(filedesc);
+                    printf("Se creo el archivo: %s\n", buffer);
+                }
+            }
+        }
+}
 }
 
 
 int main(){
 
-    my_mkdir("Cartas");
-    //my_rmdir("Cartas");
+    createDeck();    
 
     return 0;
 }
