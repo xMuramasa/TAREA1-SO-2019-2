4ce588e1b61623b5e3b24169ff355bba1c7decce
Martin Salinas Scussolin <marthoxx@gmail.com>
2019-09-28 14:01:38 -0300

    nuevas carpetas para mayor orden, todos los cambios se hacen correctamente

---
+++ include/header.h
@@ -0,0 +1,12 @@
+void myRmdir(char* nombre_carpeta);
+void myMkdir(char* nombre_carpeta);
+void checkCreate(char* buffer);
+void checkMove(char* buffer);
+void createDeck();
+void myRemoveFile(char* fileName, char* fileSrc);
+void moveFileToFolder(char* fileName, char* fileSrc, char* fileDest);
+void createTest();
+void listDirectory(char* dName);
+void test(char* dName);
+void Draw(char* dName, int r);
+void test3(char* dName);
---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "../include/header.h"
+
+int main(){
+
+//comment
+    createTest();
+    //createDeck();
+    //myMkdir("Drop");
+    myMkdir("../outfiles/testHand");
+    test3("../outfiles/Test");
+    //listDirectory("Test");
+    //listDirectory("Deck");
+
+    //moveFileToFolder("verde_+2_1.txt", "Deck", "Drop");
+
+    return 0;
+}
