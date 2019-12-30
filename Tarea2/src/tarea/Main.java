8d4e808ea9c093a92e96b76c5e64cba3cc8286a7
Martin Salinas Scussolin <marthoxx@gmail.com>
2019-12-30 01:05:55 -0300

    ya su ejecucion general de la tarea por consola

--- Tarea2/src/tarea/Main.java
+++ Tarea2/src/tarea/Main.java
@@ -1,8 +1,36 @@
 package tarea;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("xd");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Seleccione parte de la tarea a correr:");
+        System.out.println("[0] Parte 1\n[1] Parte 2");
+        
+        boolean flag = true;
+        
+        while(flag){
+            
+        int usrInput = input.nextInt();
+
+        System.out.println("Ha seleccionado " + usrInput);
+
+        if(usrInput == 0){
+            Parte2();
+            Parte1();
+            flag = false;
+        }
+        else if(usrInput == 1){
+            Parte2();
+            Parte1();
+            flag = false;
+        }
+        else{
+            System.out.println("Seleccione nuevamente");
+        }
+        }
     }
 
 }
