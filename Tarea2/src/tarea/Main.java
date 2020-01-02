f173f2bafa12dc7d3c24d4c9353da9fb967b48be
Martin Salinas Scussolin <marthoxx@gmail.com>
2020-01-02 19:45:54 -0300

    aasa

--- Tarea2/src/tarea/Main.java
+++ Tarea2/src/tarea/Main.java
@@ -1,42 +1,40 @@
 package tarea;
+
 import java.util.Scanner;
 
+public class Main {
 
-public class Main{
-    
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Parte1 p1 = new Parte1();
-        Parte2 p2 = new Parte2();
+		Parte1 p1 = new Parte1();
+		Parte2 p2 = new Parte2();
 
-        Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
-        System.out.println("Seleccione parte de la tarea a correr:");
-        System.out.println("[1] Parte 1\n[2] Parte 2");
-        
-        boolean flag = true;
-        
-        while(flag){
-            
-        int usrInput = input.nextInt();
+		System.out.println("Seleccione parte de la tarea a correr:");
+		System.out.println("[1] Parte 1\n[2] Parte 2");
 
-        System.out.println("Ha seleccionado " + usrInput);
+		boolean flag = true;
 
-        if(usrInput == 1){
-            p1.Parte_1();
-            //p2.Parte_2();
-            flag = false;
-        }
-        else if(usrInput == 2){
-            p2.Parte_2();
-            //p1.Parte_1();
-            flag = false;
-        }
-        else{
-            System.out.println("Seleccione nuevamente");
-        }
-        }
-        input.close();
-    }
+		while (flag) {
+
+			int usrInput = input.nextInt();
+
+			System.out.println("Ha seleccionado " + usrInput);
+
+			if (usrInput == 1) {
+				p1.Parte_1();
+				// p2.Parte_2();
+				flag = false;
+			} else if (usrInput == 2) {
+				p2.Parte_2();
+				// p1.Parte_1();
+				flag = false;
+			} else {
+				System.out.println("Seleccione nuevamente");
+			}
+		}
+		input.close();
+	}
 
 }
--- Tarea2/src/tarea/Parte2.java
+++ Tarea2/src/tarea/Parte2.java
@@ -4,193 +4,114 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
-class MyThread extends Thread{
+class MyThread extends Thread {
 
     int low, high;
     List<Long> arr;
 
-    public MyThread(List<Long> S, int A, int D){
-        this.arr =  S;
-        this.low =  A;
+    public MyThread(List<Long> S, int A, int D) {
+        this.arr = S;
+        this.low = A;
         this.high = D;
     }
 
     public int particion(List<Long> array, int low, int high) {
 
         Long pivote = array.get(high);
-        System.out.println("Pivot: " + pivote);
+        //System.out.println("Pivot: " + pivote);
 
         int i = (low - 1);
-		Long temp1,temp2;
-		
-		//System.out.println("Array en PART ANTES: " + arr.toString());
-        
-		
+        Long temp1, temp2;
+
+        // System.out.println("Array en PART ANTES: " + arr.toString());
+
         for (int j = low; j < high; j++) {
             if (array.get(j) < pivote) {
                 i++;
                 temp1 = array.get(i);
                 temp2 = array.get(j);
-                array.set(i,temp2);
-                array.set(j,temp1);
+                array.set(i, temp2);
+                array.set(j, temp1);
             }
         }
-		
-        temp1 = array.get(i+1);
+
+        temp1 = array.get(i + 1);
         temp2 = array.get(high);
-        array.set(i+1, temp2);
+        array.set(i + 1, temp2);
         array.set(high, temp1);
-		
-		//System.out.println("Array en PART DESP: " + arr.toString());
+
+        // System.out.println("Array en PART DESP: " + arr.toString());
 
         return i + 1;
     }
-    
+
     public void run() {
-		recursion(this.arr, this.low, this.high);
-	}
+        recursion(this.arr, this.low, this.high);
+    }
 
-	public void recursion(List<Long> array, int low, int high) {
+    public void recursion(List<Long> array, int low, int high) {
 
-		if (low < high) {
-	
-			int part = particion(this.arr, this.low, this.high);
-	
-			MyThread hijoI = new MyThread(this.arr, this.low, part-1);
-			hijoI.start();
+        if (low < high) {
 
-            try{
+            int part = particion(this.arr, this.low, this.high);
+
+            MyThread hijoI = new MyThread(this.arr, this.low, part - 1);
+            hijoI.start();
+
+            try {
                 hijoI.join();
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("Me Cai hijo 1");
             }
-            
-            MyThread hijoD = new MyThread(this.arr, part+1, this.high);
-            hijoD.start();
-            
 
-            try{
+            MyThread hijoD = new MyThread(this.arr, part + 1, this.high);
+            hijoD.start();
+
+            try {
                 hijoD.join();
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("Me Cai hijo 2");
             }
-		}
+        }
 
-	}
+    }
 }
 
-public class Parte2 extends Thread{
-    
+public class Parte2 extends Thread {
 
-    public void Parte_2(){
-        
+    public void Parte_2() {
+
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.print("Ingrese cantidad de elementos del arreglo: ");
-        
+
         int n = scan.nextInt();
 
         List<Long> arr = new ArrayList<Long>();
-        
-        for(int i = 0; i < n; i++){
-			
+
+        for (int i = 0; i < n; i++) {
+
             arr.add(scan.nextLong());
-		}
-		
+        }
+
         System.out.println("Array Antes de ser ordenado: " + arr.toString());
 
-        MyThread sort = new MyThread(arr, 0, n-1);
-        
+        MyThread sort = new MyThread(arr, 0, n - 1);
+
         sort.start();
 
         try {
-			sort.join();
-		} catch (Exception e) {}
-             
+            sort.join();
+        } catch (Exception e) {
+        }
+
         System.out.println("Sorting... Beep Boop");
 
         System.out.println("Array Después de ser ordenado: " + arr.toString());
         System.out.println("funciono 2");
 
         scan.close();
-            
-	}
-	
+
+    }
+
 }
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
-
- 
